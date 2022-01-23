18e9c0c21e5a4ff70476f7e4b77e7e810a9dff25
Erin Burton <eb@flat9.dev>
2022-01-22 18:05:00 -0800

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,15 @@ import { defineConfig } from 'vite'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import react from '@vitejs/plugin-react'
 import html from 'vite-plugin-html'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin(), html({ minify: true })],
+  resolve: {
+  alias: {
+      '~': path.resolve(__dirname, './src')
+    },
+  },
   test: {
     global: true,
     environment: 'jsdom',
